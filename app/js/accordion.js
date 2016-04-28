7c854b793e69c352848ab75191baa49751ef6fee
Vladislav <v.kirbaba@gmail.com>
2016-04-28 12:46:31 +0300

    Index - banner

--- app/js/accordion.js
+++ app/js/accordion.js
@@ -21,26 +21,3 @@ jQuery(document).ready(function($) {
    
   });
 });
-$(document).ready(function() {
- 
-  $(".banner__carousel").owlCarousel({
- 
-      navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      singleItem:true,
-      navigationText: [
-      "<span class='fa fa-chevron-left'></span>",
-      "<span class='fa fa-chevron-right'></span>"
-      ],
- 
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
- 
-  });
- 
-});
