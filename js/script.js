b7b05f774634ef1768362d09d309648aa53761ca
kirill <apuc06@mail.ru>
2016-04-28 16:24:48 +0300

    contacts page

--- js/script.js
+++ js/script.js
@@ -44,3 +44,31 @@ $(document).ready(function() {
   });
  
 });
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map('map', {
+        center: [55.778544, 37.664856],
+        zoom: 16,
+        controls: [],
+
+    })  ;
+    myGeoObject = new ymaps.GeoObject({
+        // Описание геометрии.
+        geometry: {
+            type: "Point",
+            coordinates: [55.778544, 37.664856]
+        },
+        // Свойства.
+        properties: {
+            // Контент метки.
+            preset: 'islands#icon',
+            iconColor: '#0095b6'
+        }
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+
+    myMap.geoObjects
+        .add(myGeoObject);
+}
